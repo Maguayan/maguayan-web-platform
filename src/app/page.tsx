63ec727a98c1eecc9b92357d9a6c5017f3623892
Robert Angelo Marquez <ghelomarquez@gmail.com>
2024-08-13 17:05:54 +0800

    Code Cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import {
-  SignInButton,
   SignedIn,
   SignedOut,
-  UserButton
 } from '@clerk/nextjs'
 
-import Link from "next/link";
 import Dashboard from './dashboard/page';
 import LandingPage from './home/page';
 
-export default async function Home() {
+export default async function Home() {  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
