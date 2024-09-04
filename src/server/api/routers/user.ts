5233e511fb2a1c2435b4149c75aeb0ab074f44fc
Eli <eli.quinio17@gmail.com>
2024-09-04 16:12:39 +0800

    Fix database connection

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-    getById: publicProcedure
+    getById : publicProcedure
         .input(z.string())
         .query(({ ctx, input }) => {
-            return ctx.db.user.findFirst({
+            return ctx.db.user.findUnique({
                 where : {
-                    id : parseInt(input),
+                    clerkId : input,
                 },
             });
         }),
